89ee560f901ebeb60945ed76ce7ba537abf930b3
godhomeavocados <rafikur333@gmail.com>
2023-08-01 19:03:26 +0800

    Perbaikan Terbaru

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
-
-/**
- *
- * @author Administrator
- */
-public class mahasiswa {
+    public class Mahasiswa {
+    String npm,nama,prodi;
     
+    //membuat setter
+   public void setNpm (String npm){
+this.npm = npm;
 }
+   public void setNama ( String nama ){
+this.nama = nama;
+}
+    public void setProdi ( String prodi ){
+this.prodi = prodi;
+    }
+    
+    //getter
+    public String getNpm(){
+        return npm;
+    }
+    
+    public String getNama (){
+        return nama;
+    }
+    public String getProdi(){
+        return prodi;
+    }
+}
+
--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universitas;
-
-/**
- *
- * @author Administrator
- */
-public class mahasiswaBeraksi {
+public class MahasiswaBeraksi {
+    public static void main(String[] args){
+    Mahasiswa mahasiswa1 = new Mahasiswa ();
     
+    mahasiswa1.npm     ="   :  211010556    ";
+    mahasiswa1.nama    ="  :  Rafi Kurniawan";
+    mahasiswa1.prodi   =" :  Teknik Informatika";
+    
+    System.out.println("Npm " + mahasiswa1.npm);
+    System.out.println("Nama " + mahasiswa1.nama);
+    System.out.println("Prodi " + mahasiswa1.prodi);
+    
+    }
 }
